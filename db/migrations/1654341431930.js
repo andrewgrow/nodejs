526d796b5570d0d9d908f8ff88496dbcaa94c430
A Grow <a+github@gahov.com>
2022-06-04 20:12:35 +0300

    create table telegram users

--- db/migrations/1654341431930.js
+++ db/migrations/1654341431930.js
@@ -4,7 +4,18 @@ const sqlList = [
     "CREATE TABLE IF NOT EXISTS `users` " +
     "(`_id` INT AUTO_INCREMENT PRIMARY KEY, `phone` TEXT, `name` TEXT, `utc_created_at` " +
     "TIMESTAMP DEFAULT CURRENT_TIMESTAMP, `utc_updated_at` TIMESTAMP, `utc_deleted_at` TIMESTAMP) " +
-    "ENGINE = InnoDB;"
+    "ENGINE = InnoDB;",
+
+    "CREATE TABLE IF NOT EXISTS `telegram_users` (" +
+    "    `_id` INT AUTO_INCREMENT PRIMARY KEY, " +
+    "    `chat_id` TEXT," +
+    "    `first_name` TEXT," +
+    "    `last_name` TEXT," +
+    "    `username` TEXT," +
+    "    utc_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
+    "    utc_updated_at TIMESTAMP," +
+    "    utc_deleted_at TIMESTAMP" +
+    ") ENGINE = InnoDB;"
 ];
 
 module.exports = sqlList;
