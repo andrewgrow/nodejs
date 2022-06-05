c857c1fb2ba92c3a326a11d8a1ab8eb0856524dd
A Grow <a+github@gahov.com>
2022-06-05 12:55:34 +0300

    update migrations

--- db/migrations/1654341431930.js
+++ db/migrations/1654341431930.js
@@ -1,23 +1,27 @@
 'use strict';
 
 const sqlList = [
-    "CREATE TABLE IF NOT EXISTS `users` " +
-    "(`_id` INT AUTO_INCREMENT PRIMARY KEY, `phone` TEXT, `name` TEXT, " +
-    "`created_at` INT DEFAULT (UNIX_TIMESTAMP()), " +
-    "`updated_at` INT, " +
-    "`deleted_at` INT) " +
-    "ENGINE = InnoDB;",
+    "CREATE TABLE IF NOT EXISTS `users` (" +
+    "    `_id` INT AUTO_INCREMENT PRIMARY KEY," +
+    "    `phone` TEXT," +
+    "    `name` TEXT," +
+    "    `created_at` INT DEFAULT (UNIX_TIMESTAMP())," +
+    "    `updated_at` INT," +
+    "    `deleted_at` INT" +
+    ") ENGINE = InnoDB;",
 
     "CREATE TABLE IF NOT EXISTS `telegram_users` (" +
-    "    `_id` INT AUTO_INCREMENT PRIMARY KEY, " +
+    "    `_id` INT AUTO_INCREMENT PRIMARY KEY," +
+    "    `user_id` INT," +
     "    `chat_id` TEXT," +
     "    `first_name` TEXT," +
     "    `last_name` TEXT," +
     "    `username` TEXT," +
-    "    created_at INT DEFAULT (UNIX_TIMESTAMP())," +
-    "    updated_at INT," +
-    "    deleted_at INT" +
-    ") ENGINE = InnoDB;"
+    "    utc_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
+    "    utc_updated_at TIMESTAMP," +
+    "    utc_deleted_at TIMESTAMP," +
+    "    FOREIGN KEY (`user_id`) REFERENCES `users` (`_id`)" +
+    "    ) ENGINE = InnoDB;"
 ];
 
 module.exports = sqlList;
