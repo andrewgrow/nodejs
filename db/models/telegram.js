838bcb78861a3ff0624071e1c238fb352df8e91b
A Grow <a+github@gahov.com>
2022-07-13 18:25:34 +0300

    update telegram.test.js (2)

--- db/models/telegram.js
+++ db/models/telegram.js
@@ -10,8 +10,8 @@ function getChatByLocalId(localId) {
     return mysql.getById(mysql.tables.TELEGRAM_USERS_TABLE, localId);
 }
 
-function getChatBy(chat_id) {
-    return mysql.getBy(mysql.tables.TELEGRAM_USERS_TABLE, 'chat_id', chat_id);
+function getChatBy(chatUid) {
+    return mysql.getBy(mysql.tables.TELEGRAM_USERS_TABLE, 'chat_id', chatUid);
 }
 
 function getChatsList() {
@@ -27,13 +27,15 @@ async function isDbDisconnected() {
     return await mysql.isDbDisconnected();
 }
 
-function createTelegramUser(userId, chatId) {
-    if (userId === null || userId === undefined || chatId === null || chatId === undefined) {
-        throw new Error('Cannot create a telegram chat with an user invalid data.');
-    }
+function createTelegramUser(userId, chatUid) {
     return new Promise((resolve, reject) => {
+        if (userId === null || userId === undefined || chatUid === null || chatUid === undefined) {
+            reject(new Error('Cannot create a telegram chat with an user invalid data.'));
+            return null;
+        }
+
         const sql = 'SELECT * FROM `telegram_users` WHERE user_id = ? AND chat_id = ?';
-        mysql.query(sql, [userId, chatId]) // try to find exist chat
+        mysql.query(sql, [userId, chatUid]) // try to find exist chat
             .then((result) => {
                 if (result.length === 0) { // if it does not exist, make new. First try to find user
                     return mysql.getById(mysql.tables.USERS_TABLE, userId);
@@ -45,9 +47,10 @@ function createTelegramUser(userId, chatId) {
             .then((dbUser) => { // if user exist, we will create chat
                 if (dbUser !== null && dbUser !== undefined) {
                     const sql = "INSERT INTO telegram_users (`user_id`, `chat_id`) VALUES (?, ?);";
-                    return mysql.query(sql, [userId, chatId]);
+                    return mysql.query(sql, [userId, chatUid]);
                 } else {
                     reject(new Error('Cannot create a telegram chat when user does not exist.'));
+                    return null;
                 }
             }).then((result) => {
                 resolve(result.insertId);
--- test/db/models/telegram.test.js
+++ test/db/models/telegram.test.js
@@ -2,18 +2,19 @@
 
 const telegramModel = require('../../../db/models/telegram');
 const userFabric = require('../../factories/UserFabric');
-const chatValue = '123456789AA';
+const chatUid = '123456789AA';
 let testUser, testTgChat;
 
 describe('test ../db/models/telegram.js', function () {
     before('create test user', async function () {
         testUser = await userFabric.createUserRecord();
-        testTgChat = await userFabric.createTelegramChatForUser(testUser.user_id, chatValue);
+        testTgChat = await userFabric.createTelegramChatForUser(testUser.user_id, chatUid);
     });
 
     describe('test function getChatBy()', function () {
         it('should be found by value', function () {
-            const dbTgUser = telegramModel.getChatBy(chatValue).then((tgUser) => { return JSON.stringify(tgUser) });
+            const dbTgUser = telegramModel.getChatBy(chatUid)
+                .then((tgUser) => { return JSON.stringify(tgUser) });
             return assert.eventually.equal(dbTgUser, JSON.stringify(testTgChat));
         });
         it('should be null when chatId is wrong', function () {
@@ -29,25 +30,29 @@ describe('test ../db/models/telegram.js', function () {
     describe('test function getChatByLocalId()', function () {
         it('should be created', function () {
             const id = testTgChat._id;
-            const dbTgUser = telegramModel.getChatByLocalId(id).then((tgUser) => { return tgUser._id });
+            const dbTgUser = telegramModel.getChatByLocalId(id)
+                .then((tgUser) => { return tgUser._id });
             return assert.eventually.equal(dbTgUser, id);
         });
     });
 
     describe('test function getChatsList()', function () {
         it('should be 1 or more', function () {
-            const tgUsersList = telegramModel.getChatsList().then((list) => { return list.length });
+            const tgUsersList = telegramModel.getChatsList()
+                .then((list) => { return list.length });
             return assert.eventually.isAtLeast(tgUsersList, 1);
         });
     });
 
     describe('test function getChatsListByUser()', function () {
         it('should be 1 or more', function () {
-            const tgUsersList = telegramModel.getChatsListByUser(1).then((list) => { return list.length });
+            const tgUsersList = telegramModel.getChatsListByUser(1)
+                .then((list) => { return list.length });
             return assert.eventually.isAtLeast(tgUsersList, 1);
         });
         it('should be 0', function () {
-            const tgUsersList = telegramModel.getChatsListByUser(123465789).then((list) => { return list.length });
+            const tgUsersList = telegramModel.getChatsListByUser(123465789)
+                .then((list) => { return list.length });
             return assert.eventually.equal(tgUsersList, 0);
         });
     });
@@ -57,4 +62,26 @@ describe('test ../db/models/telegram.js', function () {
             return assert.eventually.equal(telegramModel.isDbDisconnected(), false);
         });
     })
+
+    describe('test createTelegramUser()', function () {
+       it('should be rejected if user is null', function () {
+           return assert.isRejected(telegramModel.createTelegramUser(null, chatUid));
+       });
+       it('should be rejected if chatUid is null', function () {
+           return assert.isRejected(telegramModel.createTelegramUser(1, null));
+       });
+       it('should be return the same telegramUser._id if this tgUser exists', function () {
+           const beforeCreatedId = 1;
+           return assert.eventually.equal(
+               telegramModel.createTelegramUser(1, chatUid), beforeCreatedId
+           );
+       });
+        it('should be return new telegramUser._id if chatUid is new', function () {
+            const expectCreatedId = 2;
+            const testChatUid = 'testChatUid';
+            return assert.eventually.equal(
+                telegramModel.createTelegramUser(1, testChatUid), expectCreatedId
+            );
+        });
+    });
 });
