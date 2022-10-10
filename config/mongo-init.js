96711304578a9d404a897773b79fce2d9c8e7ff7
A Grow <a+github@gahov.com>
2022-10-10 15:28:23 +0300

    add docker compose build with mongodb

---
+++ config/mongo-init.js
@@ -0,0 +1,9 @@
+conn = new Mongo();
+db = conn.getDB("divo");
+
+/** Examples for creating via this init script
+db.myCollectionName.createIndex({ "address.zip": 1 }, { unique: false });
+
+db.myCollectionName.insert({ "address": { "city": "Paris", "zip": "123" }, "name": "Mike", "phone": "1234" });
+db.myCollectionName.insert({ "address": { "city": "Marsel", "zip": "321" }, "name": "Helga", "phone": "4321" });
+*/
