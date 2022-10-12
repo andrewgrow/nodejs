3ed2ee1c55d161921062cfd5eb96cfc27a57eb55
Andrew Gahov <andrew.gahov@gmail.com>
2022-10-12 23:09:11 +0300

    add comments

--- src/nest/config/db.mongo.config.ts
+++ src/nest/config/db.mongo.config.ts
@@ -1,6 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
-// @returns MongooseModuleOptions
+/**
+ * Registers the configuration object behind a specified token ('db_mongo_config').
+ * @returns MongooseModuleOptions
+ */
 export default registerAs('db_mongo_config', () => ({
     uri: process.env.MONGO_URI,
 }));
--- src/nest/config/mongodb.config.service.ts
+++ src/nest/config/mongodb.config.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MongooseModuleOptions, MongooseOptionsFactory } from '@nestjs/mongoose';
 
+/**
+ * Create the configuration object via 'db_mongo_config' token.
+ * @see ./db.mongo.config.ts
+ */
 @Injectable()
 export class MongodbConfigService implements MongooseOptionsFactory {
 
