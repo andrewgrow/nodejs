8b931868a63e156c14b5058b81e03f643ba6f91f
Andrew Gahov <andrew.gahov@gmail.com>
2023-01-25 14:29:00 +0200

    rate limit with Throttler Guard

--- src/config/app.config.module.ts
+++ src/config/app.config.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { configuration, validationSchema } from './configuration';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
     imports: [
@@ -19,7 +21,17 @@ import { MongooseModule } from '@nestjs/mongoose';
             }),
             inject: [ConfigService],
         }),
+        ThrottlerModule.forRoot({
+            ttl: 60, // the number of seconds that each request will last in storage
+            limit: 10, // the maximum number of requests within the TTL limit
+        }),
     ],
-    exports: [ConfigModule, MongooseModule],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard,
+        },
+    ],
+    exports: [ConfigModule, MongooseModule, ThrottlerModule],
 })
 export class AppConfigModule {}
--- src/divo-server.ts
+++ src/divo-server.ts
@@ -1,4 +1,4 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
