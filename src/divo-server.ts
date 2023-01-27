bd59ce20c944b4e15b051ee95273e93c7c46c774
Andrew Gahov <andrew.gahov@gmail.com>
2023-01-27 20:05:45 +0200

    enable Pages controller and serve static resources, disable sqagger in production

--- src/divo-server.ts
+++ src/divo-server.ts
@@ -4,16 +4,37 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { join } from 'path';
 import helmet from 'helmet';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as process from 'process';
 
-async function bootstrap() {
+async function startApplication(): Promise<void> {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-    app.use(helmet());
+    enableView(app);
+    enableSwagger(app);
 
-    app.useStaticAssets(join(__dirname, '../..', 'static'));
+    await app.listen(3000);
+}
+
+// starting!
+startApplication().then();
+
+/**
+ * Enable view engine and some setting for public and static folders.
+ */
+function enableView(app: NestExpressApplication) {
+    app.use(helmet());
     app.setBaseViewsDir(join(__dirname, '../..', 'views'));
     app.setViewEngine('hbs');
+}
 
+/**
+ * In production swagger have to be disabled for security reasons.
+ */
+function enableSwagger(app: NestExpressApplication): void {
+    // running development only
+    if (process.env.NODE_ENV !== 'development') {
+        return;
+    }
     const swaggerConfig = new DocumentBuilder()
         .setTitle('Divo Server')
         .setDescription('The server API description')
@@ -21,7 +42,4 @@ async function bootstrap() {
         .build();
     const document = SwaggerModule.createDocument(app, swaggerConfig);
     SwaggerModule.setup('swagger-docs', app, document);
-
-    await app.listen(3000);
 }
-bootstrap();
--- src/pages/pages.controller.ts
+++ src/pages/pages.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get, Render } from '@nestjs/common';
 @Controller()
 export class PagesController {
     @Get()
-    @Render('index')
+    @Render('mainPage')
     mainPage() {
         return { message: 'Hello world!' };
     }
